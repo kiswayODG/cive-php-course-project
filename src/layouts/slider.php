b0d0d570b23735a6f7d41cb34fa59c9080e4f86e
kiswayODG <ulrichouedraogo500@gmail.com>
2023-11-29 09:05:35 +0000

    template setting

---
+++ src/layouts/slider.php
@@ -0,0 +1,114 @@
+<section class="slider">
+    <div class="hero-slider">
+
+        <div class="single-slider" style="background-image:url('img/slider2.jpg')">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="text">
+                            <h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
+                            <div class="button">
+                                <a href="#" class="btn">Get Appointment</a>
+                                <a href="#" class="btn primary">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="single-slider" style="background-image:url('img/slider.jpg')">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="text">
+                            <h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
+                            <div class="button">
+                                <a href="#" class="btn">Get Appointment</a>
+                                <a href="#" class="btn primary">About Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="single-slider" style="background-image:url('img/slider3.jpg')">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="text">
+                            <h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
+                            <div class="button">
+                                <a href="#" class="btn">Get Appointment</a>
+                                <a href="#" class="btn primary">Conatct Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="schedule">
+    <div class="container">
+        <div class="schedule-inner">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-12 ">
+                    
+                    <div class="single-schedule first">
+                        <div class="inner">
+                            <div class="icon">
+                                <i class="fa fa-ambulance"></i>
+                            </div>
+                            <div class="single-content">
+                                <span>Partner</span>
+                                <h4>About SSPU</h4>
+                                <p>Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et erat in lacus convallis sodales.</p>
+                                <a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    
+                    <div class="single-schedule middle">
+                        <div class="inner">
+                            <div class="icon">
+                                <i class="icofont-prescription"></i>
+                            </div>
+                            <div class="single-content">
+                                <span>Partner</span>
+                                <h4>About UNB</h4>
+                                <p>Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et erat in lacus convallis sodales.</p>
+                                <a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-12">
+                    <div class="single-schedule last">
+                        <div class="inner">
+                            <div class="icon">
+                                <i class="icofont-ui-clock"></i>
+                            </div>
+                            <div class="single-content">
+                                <span>Partner</span>
+                                <h4> About Huawei</h4>
+                                <p>Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et erat in lacus convallis sodales.</p>
+                                <a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
+                            
+                                <a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ src/layouts/style_dependancies.php
@@ -0,0 +1,23 @@
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="../resources/styles/bootstrap.min.css">
+    <!-- Nice Select CSS -->
+    <link rel="stylesheet" href="../resources/styles/nice-select.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="../resources/styles/font-awesome.min.css">
+    <!-- icofont CSS -->
+    <link rel="stylesheet" href="../resources/styles/icofont.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" href="../resources/styles/slicknav.min.css">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="../resources/styles/owl-carousel.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="../resources/styles/animate.min.css">
+    <!-- Magnific Popup CSS -->
+    <link rel="stylesheet" href="../resources/styles/magnific-popup.css">
+
+    <link rel="stylesheet" href="../resources/styles/normalize.css">
+    <link rel="stylesheet" href="../resources/styles/style.css">
+    <link rel="stylesheet" href="../resources/styles/responsive.css">

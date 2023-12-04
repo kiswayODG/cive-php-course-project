1693f5095669a214e0e790062edf7c596a3cd75e
kiswayODG <ulrichouedraogo500@gmail.com>
2023-12-04 05:19:02 +0000

    settings template

---
+++ src/layouts/administration/sideBar.php
@@ -0,0 +1,32 @@
+<!-- Sidebar Start -->
+<div class="sidebar pe-4 pb-3">
+            <nav class="navbar bg-light navbar-light">
+                <a href="index.html" class="navbar-brand mx-4 mb-3">
+                    <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>ADMIN</h3>
+                </a>
+                <div class="d-flex align-items-center ms-4 mb-4">
+
+                    <div class="ms-3">
+                        <h6 class="mb-0">LUBAN</h6>
+                        <span>Workshop</span>
+                    </div>
+                </div>
+                <div class="navbar-nav w-100">
+                    <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+                    <a href="index.html" class="nav-item nav-link"><i class="fas fa-user-friends me-2 "></i>Users</a>
+
+
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-comment-alt me-2"></i>Communications</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="button.html" class="dropdown-item">News class</a>
+                            <a href="typography.html" class="dropdown-item">News</a>
+                        </div>
+                    </div>
+                    <a href="#" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Courses</a>
+                    <a href="#" class="nav-item nav-link"><i class="far fa-file-alt me-2"></i>Documents</a>
+                    <a href="#" class="nav-item nav-link"><i class="fas fa-sign-out-alt me-2"></i>Sign-out</a>
+                </div>
+            </nav>
+        </div>
+        <!-- Sidebar End -->
